da7c55077fc424a1b3162dbbe3bdc78b86080109
Mohamed Aziz Mimoun <azizmimoun@Mohameds-MacBook-Pro.local>
2023-07-05 22:13:47 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import ListPlayer from './Components/ListPlayer';
+import { Container,Navbar } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+      <div className='play'>
+        <Container>
+      <Navbar expand="lg" className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand href="#">****FIFA Players****</Navbar.Brand>
+        </Container>
+      </Navbar>
+    </Container>
+          <ListPlayer/>
+          <Container>
+      <Navbar expand="lg" className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand href="#">****Best FIFA Players Ever****</Navbar.Brand>
+        </Container>
+      </Navbar>
+    </Container>
+      </div>
+   
   );
 }
 
